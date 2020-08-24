eafb5372e8f36202ff23b71ee7216b577687faf0
Noa Rabin-Cohen <66642491+cohenoa33@users.noreply.github.com>
2020-08-23 19:09:11 -0700

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
-// Write your code here!
+// Write your coconst newHeader = document.createElement("h1");
+const newHeader = document.createElement("h1");
+newHeader.id = "victory"
+newHeader.innerHTML = "Noa is the champion!";
+document.getElementById("main").remove();
